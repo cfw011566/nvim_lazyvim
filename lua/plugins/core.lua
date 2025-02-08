67bf4ca76488a22cee2ce935e3b547ac5468477f
Ching-Feng Wang <chingfeng.wang@gmail.com>
2025-02-08 19:42:27 +0800

    add zig and change options

---
+++ lua/plugins/core.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+  {
+    "nvim-lspconfig",
+    opts = {
+      inlay_hints = { enabled = false },
+    },
+  },
+}
